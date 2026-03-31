c222b259f98f3238cf51f1284ff3eeca17194de1
dennisresearchnote <91735325+dennisresearchnote@users.noreply.github.com>
2026-03-31 08:58:28 +0000

    fix: 설명

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774942959',resources: [
+    cacheName: 'chirpy-1774947410',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
